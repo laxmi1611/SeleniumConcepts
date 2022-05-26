24e0acc5f461e773625df46cecbeac7680595682
u1124545 <u1124545@INBGL1WLX48064.quintiles.net>
2022-05-26 11:11:42 +0530

    waits

---
+++ SeleniumConcepts/src/com/brillio/fb/CreateAccountTest.java
@@ -0,0 +1,37 @@
+package com.brillio.fb;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+
+public class CreateAccountTest {
+
+	public static void main(String[] args) {
+		
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\U1124545\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+       driver.get("https://www.facebook.com/");
+       
+       driver.findElement(By.linkText("Create New Account")).click();
+       driver.findElement(By.name("firstname")).sendKeys("john");
+       driver.findElement(By.name("lastname")).sendKeys("wick");
+       driver.findElement(By.id("password_reg_input")).sendKeys("welcome123");
+       
+       Select selectDay=new Select(driver.findElement(By.id("day")));
+       selectDay.deselectByVisibleText("15");
+       
+       driver.findElement(By.xpath("//input[@value='-1']")).click();
+       driver.findElement(By.name("websubmit")).click();
+
+	}
+
+}
